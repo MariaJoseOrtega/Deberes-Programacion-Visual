c42a5265ad5c93d0790d62856c30a27938be0d16
MariaJoseOrtega <mjp.ortega@yavirac.edu.ec>
2021-06-28 11:34:50 -0500

    subiendo clase de la semana 1 y 2

---
+++ SEMANA 1/Persona.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace myapp
+{
+    class Persona
+    {
+        public string nombre;
+        public int edad;
+        public float peso;
+        public bool esAficionadoAlFutbol;
+        public DateTime fechaDeCreacionRegistro;
+        public float estatura;
+        public string deporteFavorito;
+        public string pasatiempoFavorito;
+        public double sueldoMensual;
+        public string semestre;
+
+    }
+
+
+
+
+
+
+}
